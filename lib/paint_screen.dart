5d837b2dd0380eb9966d888e00349869033eb125
umang0620 <120244531+umang0620@users.noreply.github.com>
2023-04-30 16:46:18 +0530

    complete

--- lib/paint_screen.dart
+++ lib/paint_screen.dart
@@ -1,9 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:skribbl/models/touch_points.dart';
+import 'package:skribbl/sidebar/player_scoreboard_drawer.dart';
+import 'package:skribbl/waiting_lobby_screen.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'final_leaderboard.dart';
+import 'home_screen.dart';
 import 'models/my_custom_painter.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'dart:convert';
 
 
 
@@ -25,6 +32,19 @@ class _PaintScreenState extends State<PaintScreen> {
   Color selectedColor = Colors.black;
   double opacity = 1;
   double strokeWidth = 2;
+  List<Widget> textBlankWidget = [];
+  ScrollController _scrollController = ScrollController();
+  TextEditingController controller = TextEditingController();
+  List<Map> messages = [];
+  int guessedUserCtr = 0;
+  int _start = 60;
+  late Timer _timer;
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+  List<Map> scoreboard = [];
+  bool isTextInputReadOnly = false;
+  int maxPoints = 0;
+  String winner = "";
+  bool isShowFinalLeaderboard = false;
 
 
   @override
@@ -33,9 +53,36 @@ class _PaintScreenState extends State<PaintScreen> {
     super.initState();
     connect();
   }
+
+  void startTimer() {
+    const oneSec = const Duration(seconds: 1);
+    _timer = Timer.periodic(oneSec, (Timer time) {
+      if (_start == 0) {
+        socket.emit('change-turn', dataofRoom['name']);
+        setState(() {
+          _timer.cancel();
+        });
+      } else {
+        setState(() {
+          _start--;
+        });
+      }
+    });
+  }
+
+
+  void renderTextBlank(String text){
+         textBlankWidget.clear();
+         for(int i=0; i < text.length; i++)
+           {
+             textBlankWidget.add(Text('_',style: TextStyle(fontSize: 30)));
+           }
+  }
+
+
   void connect(){
     print("ASDASD");
-    socket = IO.io("http://192.168.1.6:3000", <String, dynamic>{
+    socket = IO.io("http://192.168.1.11:3000", <String, dynamic>{
       'transports':['websocket'],
       'autoConnect': true,
     });
@@ -49,18 +96,37 @@ class _PaintScreenState extends State<PaintScreen> {
     }
     socket.onConnect((data) {
       print("Connected");
-      ;
       socket.on('updateRoom', (roomData) {
+          print(roomData['word']);
            setState(() {
+             renderTextBlank(roomData['word']);
              dataofRoom = roomData;
            });
            if(roomData['isJoin'] != true)
              {
-               //start timmer
+              startTimer();
              }
+           scoreboard.clear();
+          for (int i = 0; i < roomData['players'].length; i++) {
+            setState(() {
+              scoreboard.add({
+                'username': roomData['players'][i]['nickname'],
+                'points': roomData['players'][i]['points'].toString()
+              });
+            });
+          }
       });
+
+      socket.on(
+          'notCorrectGame',
+              (data) => Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(builder: (context) => HomeScreen()),
+                  (route) => false));
+
       socket.on('points', (point) {
+       // print(point);
         if (point['details'] != null) {
+
           setState(() {
             points.add(TouchPoints(
                 points: Offset((point['details']['dx']).toDouble(),
@@ -73,6 +139,85 @@ class _PaintScreenState extends State<PaintScreen> {
             ));
           });
         }
+      });
+      socket.on('msg',(msgData){
+        print(msgData);
+        setState(() {
+          messages.add(msgData);
+          guessedUserCtr = msgData['guessedUserCtr'];
+        });
+        if (guessedUserCtr == dataofRoom['players'].length - 1) {
+          socket.emit('change-turn', dataofRoom['name']);
+        }
+        _scrollController.animateTo(
+            _scrollController.position.maxScrollExtent + 65,
+            duration: Duration(milliseconds: 200),
+            curve: Curves.easeInOut);
+
+      });
+
+      socket.on('change-turn', (data){
+        String oldWord = dataofRoom['word'];
+        showDialog(context: context, builder:(context){
+          Future.delayed(Duration(seconds: 3), (){
+            setState(() {
+              dataofRoom = data;
+              renderTextBlank(data['word']);
+              isTextInputReadOnly = false;
+              guessedUserCtr = 0;
+              _start = 60;
+              points.clear();
+            });
+            Navigator.of(context).pop();
+            _timer.cancel();
+            startTimer();
+          });
+          return AlertDialog(
+              title: Center(child: Text('Word was $oldWord'),));
+        });
+      });
+
+      socket.on('updateScore', (roomData) {
+        scoreboard.clear();
+        for (int i = 0; i < roomData['players'].length; i++) {
+          setState(() {
+            scoreboard.add({
+              'username': roomData['players'][i]['nickname'],
+              'points': roomData['players'][i]['points'].toString()
+            });
+          });
+        }
+      });
+
+      socket.on('show-leaderboard',(roomPlayers){
+        scoreboard.clear();
+        for (int i = 0; i < roomPlayers.length; i++) {
+          setState(() {
+            scoreboard.add({
+              'username': roomPlayers[i]['nickname'],
+              'points': roomPlayers[i]['points'].toString()
+            });
+            print(scoreboard);
+            print("gffffffffffffffffffffffffff");
+            print(maxPoints);
+            print("gffffffffffffffffffffffffff");
+            print(int.parse(scoreboard[i]['points']));
+          });
+
+          if (maxPoints < int.parse(scoreboard[i]['points'])) {
+             winner = scoreboard[i]['username'];
+            maxPoints = int.parse(scoreboard[i]['points']);
+          }
+          else if (int.parse(scoreboard[i]['points']) == 0)
+            {
+              winner = scoreboard[i]['username'];
+              maxPoints = int.parse(scoreboard[i]['points']);
+            }
+        }
+        setState(() {
+          _timer.cancel();
+          isShowFinalLeaderboard = true;
+        });
       });
 
       socket.on('color-change', (colorString) async {
@@ -89,7 +234,39 @@ class _PaintScreenState extends State<PaintScreen> {
         });
       });
 
+      socket.on('clear-screen', (data) {
+        setState(() {
+          points.clear();
+        });
+      });
+
+      socket.on('closeInput', (_) {
+        socket.emit('updateScore', widget.data['name']);
+        setState(() {
+          isTextInputReadOnly = true;
+        });
+      });
+
+      socket.on('user-disconnected', (data) {
+        scoreboard.clear();
+        print(data);
+        for (int i = 0; i < data['players'].length; i++) {
+          setState(() {
+            scoreboard.add({
+              'username': data['players'][i]['nickname'],
+              'points': data['players'][i]['points'].toString()
+            });
+          });
+        }
+      });
     });
+  }
+
+  @override
+  void dispose() {
+    socket.dispose();
+    _timer.cancel();
+    super.dispose();
   }
 
   @override
@@ -97,7 +274,6 @@ class _PaintScreenState extends State<PaintScreen> {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
 
-    //
     void selectColor()
     {
        showDialog(context: context, builder: (context) => AlertDialog(
@@ -125,8 +301,13 @@ class _PaintScreenState extends State<PaintScreen> {
        ));
     }
     return Scaffold(
+      key:scaffoldKey,
+       drawer: PlayerScore(scoreboard),
        backgroundColor: Colors.white,
-      body: Stack(
+      // body: Center(child: Text(JsonEncoder().convert(widget.data))),
+      body: dataofRoom != null ?
+      dataofRoom['isJoin'] != true ?
+      !isShowFinalLeaderboard ? Stack(
         children: [
           Column(
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -137,7 +318,7 @@ class _PaintScreenState extends State<PaintScreen> {
                 height: height*0.55,
                 child: GestureDetector(
                   onPanUpdate: (details){
-                    print(details.localPosition.dx);
+                   // print(details.localPosition.dx);
                     socket.emit('paint',{
                       'details':{
                         'dx':details.localPosition.dx,
@@ -148,7 +329,7 @@ class _PaintScreenState extends State<PaintScreen> {
 
                   },
                   onPanStart: (details){
-                    print(details.localPosition.dx);
+                  //  print(details.localPosition.dx);
                     socket.emit('paint',{
                       'details':{
                            'dx':details.localPosition.dx,
@@ -198,13 +379,104 @@ class _PaintScreenState extends State<PaintScreen> {
                      },
                    ),
                    ),
-                   IconButton(onPressed: (){}, icon:Icon(Icons.layers_clear,color: selectedColor,)),
+                   IconButton(onPressed: (){
+                     socket.emit(
+                         'clean-screen', dataofRoom['name']);
+                   }, icon:Icon(Icons.layers_clear,color: selectedColor,)),
                 ],
               ),
+              dataofRoom['turn']['nickname'] != widget.data['nickname'] ? Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: textBlankWidget,
+              ) :
+              Center(
+                child: Text(dataofRoom['word'],
+                  style: TextStyle(fontSize: 30),),),
+              // Displaying messages
+              Container(
+                height: MediaQuery.of(context).size.height*0.3,
+                child:ListView.builder(
+                  controller: _scrollController,
+                    shrinkWrap: true,
+                    itemCount: messages.length,
+                    itemBuilder: (context, index){
+                    var msg = messages[index].values;
+                    return ListTile(
+                      title: Text(
+                        msg.elementAt(0),
+                        style: TextStyle(color: Colors.black,fontSize: 19, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        msg.elementAt(1),
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      ),
+                    );
+                    })
+              ),
             ],
-          )
+          ),
+            dataofRoom['turn']['nickname'] != widget.data['nickname'] ? Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                readOnly: isTextInputReadOnly,
+                controller: controller,
+                onSubmitted: (value){
+                  if(value.trim().isNotEmpty)
+                    {
+                      Map map = {
+                        'username':widget.data['nickname'],
+                        'msg': value.trim(),
+                        'word':dataofRoom['word'],
+                        'roomName': widget.data['name'],
+                        'guessedUserCtr': guessedUserCtr,
+                        'totalTime': 60,
+                        'timeTaken': 60 - _start,
+                      };
+                      socket.emit('msg',map);
+                      controller.clear();
+                    }
+                },
+                autocorrect: false,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide:const  BorderSide(color: Colors.transparent),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide:const  BorderSide(color: Colors.transparent),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
+                    filled: true,
+                    fillColor: Color(0xffF5F5FA),
+                    hintText: 'Your Guess',
+                    hintStyle: const TextStyle(fontSize: 14,
+                        fontWeight: FontWeight.w400)
+                ),
+                textInputAction: TextInputAction.done,
+              ),
+            ),
+          ) : Container(),
+          SafeArea(child: IconButton(icon:Icon(Icons.menu , color: Colors.black,),
+            onPressed: () => scaffoldKey.currentState!.openDrawer(),),),
         ],
+      ): FinalLeaderboard(scoreboard,winner)
+          : WaitingLobbyScreen(lobbyName: dataofRoom['name'],
+          noOfPlayers: dataofRoom['players'].length,
+          occupancy: dataofRoom['occupancy'],
+          players: dataofRoom['players'],)
+          : Center(child: CircularProgressIndicator(),),
+    floatingActionButton: Container(
+      margin: EdgeInsets.only(bottom: 30),
+      child: FloatingActionButton(
+        onPressed: (){},
+        elevation: 7,
+        backgroundColor: Colors.white,
+        child: Text('$_start',style: TextStyle(color: Colors.black,fontSize: 22),),
       ),
+    ),
     );
   }
 }
--- server/index.js
+++ server/index.js
@@ -47,14 +47,14 @@ io.on('connection', (socket) => {
         room.players.push(player);
         room  = await room.save();
         socket.join(name);
-        io.to(name).emit('updateRoom',Room,ack=>{});
+        console.log(room);
+        io.to(name).emit('updateRoom',room);
     }
     catch(err)
     {
      console.log(err);
     }
     }),
-
     //JOIN GAME CALLBACK
     socket.on('join-game',async({nickname, name })=>
     {
@@ -63,11 +63,14 @@ io.on('connection', (socket) => {
     if(!room)
     {
         socket.emit('notCorrectGame','Please Enter valid Room name!');
+          // console.log("wrong room name");
         return;
 
     }
+     console.log(room);
     if(room.isJoin)
     {
+     // console.log("2");
         let player = {
             socketId: socket.id,
             nickname
@@ -80,8 +83,10 @@ io.on('connection', (socket) => {
                room.isJoin = false;
         }
         room.turn = room.players[room.turnIndex];
-        room = await room.save();
-        io.to(name).emit('updateRoom',room,ack=>{});
+        room = await
+        room.save();
+        console.log(room);
+        io.to(name).emit('updateRoom',room);
     }
     else{
         socket.emit('notCorrectGame','The game is in progress, plz try later!');
@@ -93,24 +98,139 @@ io.on('connection', (socket) => {
     }
     })
 
+socket.on('msg', async (data) => {
+        console.log("in msg event");
+        console.log(data.word);
+        try{
+          if(data.msg === data.word)
+           {
+            let room = await Room.find({name: data.roomName});
+            let userPlayer = room[0].players.filter(
+              (player) => player.nickname === data.username
+            )
+            if(data.timeTaken !== 0)
+            {
+               userPlayer[0].points += Math.round((200/ data.timeTaken) * 10);
+            }
+            room = await room[0].save();
+            io.to(data.roomName).emit('msg', {
+                username: data.username,
+                msg: 'Gussed it!',
+                guessedUserCtr: data.guessedUserCtr + 1,
+            })
+            socket.emit('closeInput', "");
+          }else{
+            io.to(data.roomName).emit('msg',{
+                    username: data.username,
+                    msg: data.msg,
+                    guessedUserCtr: data.guessedUserCtr,
+                    })
+          }
+        }
+        catch(err)
+        {
+        console.log("in catch block",err);
+        }
+})
+socket.on('change-turn', async(name) =>
+{
+    try{
+    let room = await Room.findOne({name});
+    let idx = room.turnIndex;
+    if(idx +1 === room.players.length){
+       room.currentRound +=1;
+    }
+    if(room.currentRound <= room.maxRounds){
+         const word = getWord();
+         room.word = word;
+         room.turnIndex = (idx+1) % room.players.length;
+         room.turn = room.players[room.turnIndex];
+         room = await room.save();
+         io.to(name).emit('change-turn', room);
+    }else{
+        console.log(room.player);
+        io.to(name).emit('show-leaderboard', room.players);
+    }
+    }catch(err)
+    {
+    console.log(err);
+    }
+})
+ socket.on('updateScore', async (name) => {
+        try {
+            const room = await Room.findOne({name});
+            io.to(name).emit('updateScore', room);
+        } catch(err) {
+            console.log(err);
+        }
+    })
     //White board scokets
     socket.on('paint',({details, roomName})=>
     {
-       io.to(roomName).emit('points',{details: details});
-      // socket.broadcast.emit('points', )
+    if(details != null)
+    {
+   // io.emit('points',{details: details});
+     io.to(roomName).emit('points',{details: details});
+    }
+    //   console.log(details);
     })
 
    //color socket
     socket.on('color-change',({color, roomName}) =>
     {
-      io.to(roomName).emit('color-change',color);
+       if(color != null)
+      {
+        // console.log(color,roomName);
+         io.to(roomName).emit('color-change',color);
+      }
+     // io.emit('color-change',color);
     })
 
-    //Stroke Scoket
+   // Stroke Scoket
     socket.on('stroke-width',({value, roomName}) => {
-       io.to(roomName).emit('stroke-width', value);
+       if(value != null)
+       {
+          // console.log(value,roomName);
+           io.to(roomName).emit('stroke-width', value);
+       }
+//       io.to(roomName).emit('stroke-width', value);
+//        io.emit('stroke-width', value);
     })
 
+    // Clear Screen
+    socket.on('clean-screen', (roomName) => {
+        io.to(roomName).emit('clear-screen', '');
+    })
+
+    socket.on('disconnect', async()=>{
+    console.log("your are in disconnect");
+ let room  = await Room.findOne({"players.socketId": socket.id});
+//  console.log(room);
+    try {
+       // let room  = await Room.findOne({"players.socketId": socket.id});
+         console.log(room);
+        for(let i=0; i < room.players.length; i++ )
+        {
+            if(room.players[i].socketId === socket.id)
+            {
+                    room.players.splice(i, 1);
+                    break;
+            }
+         }
+         room = await room.save();
+         if(room.players.length === 1)
+         {
+               console.log("in if brodcast")
+              io.to(room.name).emit('show-leaderboard', room.players);
+         }
+         else{
+         console.log("in else brodcast")
+          io.to(room.name).emit('user-disconnected', room);
+         }
+    }catch(err){
+         console.log(err);
+    }
+    })
 })
 
 
